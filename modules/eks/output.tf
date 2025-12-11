0f977c68dd43745a722ecf303f47ad451260e72c
unknown <maheshmahe2938@gmail.com>
2025-12-11 22:05:26 +0530

    finally

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -1,6 +1,7 @@
 output "kubeconfig_path" {
   value = var.kubeconfig_path
 }
+
 output "cluster_name" {
   value = var.cluster_name
 }
@@ -8,16 +9,11 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = aws_eks_cluster.this.endpoint
 }
+
 output "cluster_certificate" {
   value = aws_eks_cluster.this.certificate_authority[0].data
 }
+
 output "cluster_arn" {
   value = aws_eks_cluster.this.arn
-}
-output "kubeconfig_path" {
-  value = var.kubeconfig_path
-}
-
-output "cluster_name" {
-  value = var.cluster_name
 }
