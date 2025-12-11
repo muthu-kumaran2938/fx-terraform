cab06f0e45aac6b647fe88071cef4ddfc18f3b39
unknown <maheshmahe2938@gmail.com>
2025-12-11 21:52:08 +0530

    rdsvars

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,9 +1,37 @@
+###########################################################
+# Environment
+###########################################################
+variable "env" {
+  description = "Environment (dev/prod/etc)"
+  type        = string
+}
+
+###########################################################
+# Private Subnets
+###########################################################
+variable "private_subnet_ids" {
+  description = "Private subnet IDs for the RDS instances"
+  type        = list(string)
+}
+
+###########################################################
+# Security Groups
+###########################################################
+variable "db_security_group_ids" {
+  description = "Security group IDs to attach to the RDS instances"
+  type        = list(string)
+}
+
+###########################################################
+# Databases map
+###########################################################
 variable "databases" {
+  description = "Map of databases and optional parameters for each RDS instance"
   type = map(object({
     db_name             = optional(string)
     username            = optional(string)
     password            = optional(string)
-    secret_arn          = optional(string)   # NEW optional field
+    secret_arn          = optional(string)   # Use Secrets Manager if provided
     engine              = optional(string, "postgres")
     engine_version      = optional(string)
     instance_class      = optional(string)
@@ -13,32 +41,21 @@ variable "databases" {
     deletion_protection = optional(bool, false)
   }))
 }
-variable "env" {
-  type        = string
-  description = "Environment (dev/prod/etc)"
-}
 
-variable "private_subnet_ids" {
-  type        = list(string)
-  description = "Private subnets for RDS"
-}
-
-variable "db_security_group_ids" {
-  type        = list(string)
-  description = "Security groups for RDS"
-}
-
-variable "databases" {
-  type        = map(object({
-    db_name   : string
-    secret_arn: string
-  }))
-  description = "Databases and corresponding secrets"
-}
-
+###########################################################
+# Tags
+###########################################################
 variable "tags" {
+  description = "Tags to assign to all RDS resources"
   type        = map(string)
-  description = "Tags for RDS resources"
   default     = {}
 }
 
+###########################################################
+# Default engine version (if not specified per database)
+###########################################################
+variable "default_engine_version" {
+  description = "Default engine version if not specified in the database map"
+  type        = string
+  default     = "15.3"   # change as needed (Postgres example)
+}
