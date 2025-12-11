f2eb6970341b87b90f25ae232055c690010d1a09
unknown <maheshmahe2938@gmail.com>
2025-12-11 21:45:54 +0530

    added iam_instance profile

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -4,3 +4,9 @@ variable "subnet_id" {}
 variable "instance_type" { default = "t3.micro" }
 variable "sg_id" {}
 variable "key_secret_name" { default = "red-ec2" }
+# ../../modules/ec2/variables.tf
+variable "iam_instance_profile" {
+  description = "IAM instance profile to attach to EC2"
+  type        = string
+  default     = null
+}
