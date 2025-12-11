70cbd0b90fe5249f831490926cb2d456a3c26603
unknown <maheshmahe2938@gmail.com>
2025-12-11 21:40:50 +0530

    added

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -208,8 +208,20 @@ module "alb" {
 resource "aws_security_group" "endpoint_sg" {
   name = "endpoint-sg-dev"
   vpc_id = module.vpc.vpc_id
-  egress { from_port=0; to_port=0; protocol="-1"; cidr_blocks=["0.0.0.0/0"] }
-  ingress { from_port=443; to_port=443; protocol="tcp"; cidr_blocks=[module.vpc.vpc_cidr] }
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = [module.vpc.vpc_cidr]
+}
+
 }
 module "vpc_endpoints" {
   source = "../../modules/vpc-endpoint"
